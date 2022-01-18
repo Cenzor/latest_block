216eb855ec1e99eb8b4cf10c65d50f6583eda4e7
lomakov.k <lomakov.k@gmail.com>
2022-01-18 18:38:36 +1000

    docstring, typing

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ app = FastAPI()
 
 
 @app.get("/get_latest_block/")
-def get_info_from_latest_block():
+def get_info_from_latest_block() -> dict:
+    """
+    Receives a GET request.
+    :return: should return info about latest block: timestamp
+    and count of transactions.
+    """
     block_info = get_block_info()
     return {
         "Timestamp": block_info.get("timestamp"),
-        "Count of transactions in the block": block_info.get("transactions_count")
+        "Count of transactions in the block":
+            block_info.get("transactions_count")
     }
--- util.py
+++ util.py
@@ -3,22 +3,29 @@ from web3 import Web3
 from web3.middleware import geth_poa_middleware
 
 
-def get_block_info():
-
+def get_block_info() -> dict:
+    """
+    Make requests to the public Ethereum endpoints and get info 
+    about latest block. Adds info to the Elasticsearch.
+    :return: should return dictionary with timestamp and count of transactions
+    in the block
+    """
     block_info = dict()
     w3 = Web3(Web3.WebsocketProvider('wss://rinkeby-light.eth.linkpool.io/ws'))
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     block_info["timestamp"] = w3.eth.get_block('latest').get('timestamp')
-    block_info["transactions_count"] = len(w3.eth.get_block('latest').get('transactions'))
+    block_info["transactions_count"] = len(
+        w3.eth.get_block('latest').get('transactions')
+    )
 
     es = Elasticsearch()
-    index = "block_info"
-    response = es.index(index=index, id=1, document=block_info)
+    index: str = "block_info"
+    response: dict = es.index(index=index, id=1, document=block_info)
     print(response['result'])
 
     es.indices.refresh(index=index)
 
-    response = es.search(index=index, query={"match_all": {}})
+    response: dict = es.search(index=index, query={"match_all": {}})
     print("Got %d Hits:" % response['hits']['total']['value'])
     for hit in response['hits']['hits']:
         print("%(timestamp)s, %(transactions_count)s" % hit["_source"])
