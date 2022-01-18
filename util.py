e1199043d8a7f919236341089126ccf9f02e1c17
lomakov.k <lomakov.k@gmail.com>
2022-01-18 21:33:59 +1000

    get latest doc from ES

--- util.py
+++ util.py
@@ -5,8 +5,8 @@ from web3.middleware import geth_poa_middleware
 
 def get_block_info() -> dict:
     """
-    Make requests to the public Ethereum endpoints and get info 
-    about latest block. Adds info to the Elasticsearch.
+    Make requests to the public Ethereum endpoints and get info
+    about latest block.
     :return: should return dictionary with timestamp and count of transactions
     in the block
     """
@@ -18,16 +18,24 @@ def get_block_info() -> dict:
         w3.eth.get_block('latest').get('transactions')
     )
 
+    add_document_to_elastic(block_info)
+
+    return block_info
+
+
+def add_document_to_elastic(block_info: dict) -> None:
+    """
+    Adds info to the Elasticsearch.
+    """
     es = Elasticsearch()
     index: str = "block_info"
-    response: dict = es.index(index=index, id=1, document=block_info)
-    print(response['result'])
+    response: dict = es.index(index=index, document=block_info)
 
     es.indices.refresh(index=index)
 
-    response: dict = es.search(index=index, query={"match_all": {}})
-    print("Got %d Hits:" % response['hits']['total']['value'])
-    for hit in response['hits']['hits']:
-        print("%(timestamp)s, %(transactions_count)s" % hit["_source"])
-
-    return block_info
+    response: dict = es.search(index=index, query={"match_all": {}},
+                               sort={"timestamp": "desc"}, size=1)
+    documents_quantity = response['hits']['total']['value']
+    latest_block_info = response["hits"]["hits"][0].get("_source")
+    print(f"Total documents quantity: {documents_quantity}")
+    print(f"Info from latest block: {latest_block_info}")
